ee2963d165981a30009ad2802f57d9142c0485b4
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-05-26 23:30:01 +0100

    Add more sophisticated rules

--- parser.js
+++ parser.js
@@ -1,28 +1,44 @@
-const example_text = '. AO1 L | HH Y UW1 M AH0 N | B IY1 IH0 NG Z | AA1 R | B AO1 R N | \
-F R IY1 | AH0 N D | IY1 K W AH0 L | IH0 N | D IH1 G N AH0 T IY0 | AH0 N D | \
-R AY1 T S . DH EY1 | AA1 R | EH0 N D AW1 D | W IH1 DH | R IY1 Z AH0 N | \
-AH0 N D | K AA1 N SH AH0 N S | AH0 N D | SH UH1 D | AE1 K T | T AH0 W AO1 R D Z | \
-W AH1 N | AH0 N AH1 DH ER0 | IH0 N | AH0 | S P IH1 R AH0 T | AH1 V | \
+const example_text = 'AO1 L | HH Y UW1 M AH0 N | B IY1 IH0 NG Z | AA1 R | B AO1 R N | F R IY1 | \
+AH0 N D | IY1 K W AH0 L | IH0 N | D IH1 G N AH0 T IY0 | AH0 N D | R AY1 T S . | DH EY1 | AA1 R | \
+EH0 N D AW1 D | W IH1 DH | R IY1 Z AH0 N | AH0 N D | K AA1 N SH AH0 N S | AH0 N D | SH UH1 D | \
+AE1 K T | T AH0 W AO1 R D Z | W AH1 N | AH0 N AH1 DH ER0 | IH0 N | AH0 | S P IH1 R AH0 T | AH1 V | \
 B R AH1 DH ER0 HH UH2 D .'
 
-const vowels = ['AA', 'AE', 'AH', 'AO', 'AW', 'AY', 'EH', 'ER', 'EY', 'IH', 'IY',
-    'OW', 'OY', 'UH', 'UW']
+const vowels = [/AA/g, /AE/g, /AH/g, /AO/g, /AW/g, /AY/g, /EH/g, /ER/g, /EY/g, /IH/g, /IY/g, /OW/g,
+    /OY/g, /UH/g, /UW/g]
 
 function parse(text, with_stress, cutspell) {
-    let sp = text.split(' ')
     let result = ''
-    for (let i = 0; i < sp.length - 2; i++) {
-        result += convert(sp[i + 1], sp[i], sp[i + 2], with_stress, cutspell)
+    const words = text.split('|')
+    for (let word of words) {
+        let num_syllables = countVowels(word)
+        const phons = word.trim().split(' ')
+        for (let i = 0; i < phons.length; i++) {
+            result += convert(phons[i], phons[i - 1], phons[i + 1], phons[i + 2], num_syllables,
+                with_stress, cutspell)
+        }
+        result += ' '
     }
-    return result
+    return result.trim()
 }
 
-function convert(symbol, behind, ahead1, ahead2, with_stress, cutspell) {
+function countVowels(word) {
+    if (!word) {
+        return 0
+    }
+    let num_vowels = 0
+    for (let vowel of vowels) {
+        num_vowels += (word.match(vowel) || []).length
+    }
+    return num_vowels
+}
+
+function convert(symbol, behind, ahead1, ahead2, num_syllables, with_stress, cutspell) {
     let stress = symbol.substr(2, 1)
     let symbol_nos = symbol.substr(0, 2)
-    let ahead1_nos = ahead1.substr(0, 2)
+    let ahead1_nos = ahead1 ? ahead1.substr(0, 2) : ''
     function pickStress(ws, wos) {
-        if (with_stress && stress === '1') {
+        if (with_stress && stress === '1' && num_syllables >= 2) {
             return ws
         } else {
             return wos
@@ -30,15 +46,22 @@ function convert(symbol, behind, ahead1, ahead2, with_stress, cutspell) {
     }
     switch (symbol_nos) {
     case 'AA':
+        if (cutspell && ahead1 === 'R' && countVowels(ahead2) === 0) {
+            return pickStress('á', 'a')
+        }
         return pickStress('áa', 'aa')
     case 'AE':
         return pickStress('á', 'a')
     case 'AH':
-        if (cutspell && stress === '0') {
+        if (cutspell && stress === '0' && num_syllables >= 2) {
             switch (behind + '/' + ahead1) {
             case 'M/N':
             case 'W/L':
             case 'T/N':
+            case 'SH/N':
+            case 'ZH/N':
+            case 'Z/N':
+            case 'S/N':
                 return ''
             }
         }
@@ -72,10 +95,13 @@ function convert(symbol, behind, ahead1, ahead2, with_stress, cutspell) {
     case 'EH':
         return pickStress('é', 'e')
     case 'ER':
-        if (cutspell && stress === '0') {
-            switch (behind + '/' + ahead1) {
-            case 'M/|':
-            case 'N/|':
+        if (cutspell && stress === '0' && num_syllables >=2 && countVowels(ahead1) == 0) {
+            switch (behind) {
+            case 'M':
+            case 'N':
+            case 'DH':
+            case 'T':
+            case 'K':
                 return 'r'
             }
         }
@@ -96,6 +122,8 @@ function convert(symbol, behind, ahead1, ahead2, with_stress, cutspell) {
     case 'IY':
         if (ahead1_nos === 'IH' || ahead1_nos === 'IY') {
             return pickStress('íj', 'ij')
+        } else if (cutspell && !ahead1 && stress == '0') {
+            return 'i'
         }
         return pickStress('íi', 'ii')
     case 'JH':
@@ -113,6 +141,8 @@ function convert(symbol, behind, ahead1, ahead2, with_stress, cutspell) {
     case 'OW':
         if (ahead1_nos === 'UH' || ahead1_nos === 'UW') {
             return pickStress('ów', 'ow')
+        } else if (ahead1_nos === 'R' && countVowels(ahead2) == 0) {
+            return pickStress('ó', 'o')
         }
         return pickStress('óu', 'ou')
     case 'OY':
@@ -146,9 +176,7 @@ function convert(symbol, behind, ahead1, ahead2, with_stress, cutspell) {
         return 'z'
     case 'ZH':
         return 'jh'
-    case '|':
-        return ' '
     default:
-        return symbol + ' '
+        return symbol
     }
 }
