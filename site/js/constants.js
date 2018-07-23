9f53f90948353e03149a31b8e79302bbddc46565
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-07-23 12:34:17 +0100

    Define lexical sets
    Currently not used in the code, but soon!

---
+++ site/js/constants.js
@@ -0,0 +1,25 @@
+'use strict'
+
+lexicalSets = {
+    TRAP: 'a',
+    DRESS: 'e',
+    KIT: 'i',
+    STRUT: 'o',
+    FOOT: 'u',
+    LOT: 'aa',
+    FACE: 'ei',
+    FLEECE: 'ii',
+    GOAT: 'ou',
+    GOOSE: 'uu',
+    PRICE: 'ai',
+    MOUTH: 'au',
+    CHOICE: 'oi',
+    START: 'aar',
+    SQUARE: 'eir',
+    NEAR: 'iir',
+    FORCE: 'oor',
+    NURSE: 'ur',
+    LURE: 'uur',
+    happY: 'i',
+    commA: 'e'
+}
